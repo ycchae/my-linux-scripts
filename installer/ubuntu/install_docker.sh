50668c88b02b1900533a0d99922147b8c95ab6ad
Yunchang <cyc@knu.ac.kr>
2021-10-02 02:00:16 +0900

    update docker installer

--- installer/ubuntu/install_docker.sh
+++ installer/ubuntu/install_docker.sh
@@ -5,19 +5,18 @@ if [ "$USER" != "root" ];then
     echo -e "${Red}need to run with sudo!${Color_Off}"
 fi
 
-apt-get remove -y docker docker-engine docker.io containerd runc;
-apt-get update -y;
-apt-get install -y \
+sudo apt-get remove -y docker docker-engine docker.io containerd runc;
+sudo apt-get update -y;
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common ;
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - ;
-apt-key fingerprint 0EBFCD88 ;
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable" ;\
-apt-get update -y ;
-apt-get install -y docker-ce docker-ce-cli containerd.io;
+    gnupg \
+    lsb-release;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y ;
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io;
+
